9c33d576a048fedd7d970cba8645a14673896387
rhehdgus8831 <rhehdgus8831@naver.com>
2025-07-21 11:51:44 +0900

    회원 생성 및 글로벌 예외처리, 커스텀 에외처리, 열거형 에러코드 생성, 회원 생성 로직

---
+++ src/main/java/com/spring/basic/chap6/exception/custom/MemberException.java
@@ -0,0 +1,19 @@
+package com.spring.basic.chap6.exception.custom;
+
+import com.spring.basic.chap6.exception.dto.ErrorCode;
+import lombok.Getter;
+
+@Getter
+// 사용자 정의 예외 클래스 - 개발자가 원하는 스펙에 맞는 에러 클래스
+public class MemberException extends RuntimeException{
+
+    private final int status; // 에러 상태 코드와 이름을 포함
+    private final String errorName; // 에러의 이름
+
+    public MemberException(ErrorCode errorCode) {
+        super(errorCode.getErrorMessage());
+        this.errorName = errorCode.toString();
+        this.status = errorCode.getStatusCode();
+    }
+
+}
---
+++ src/main/java/com/spring/basic/chap6/exception/dto/ErrorResponse.java
@@ -0,0 +1,23 @@
+package com.spring.basic.chap6.exception.dto;
+
+
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@EqualsAndHashCode
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+// 에러 응답 시 사용할 형식있는 객체
+public class ErrorResponse {
+
+    private LocalDateTime timestamp ; // 에러가 발생한 시간
+    private int status; // 에러 상태코드
+    private String error; // 에러의 이름
+    private String message; // 에러의 원인 메세지
+    private String path; // 에러가 발생한 URL 정보
+}
